fbf7b0cd95e1ef2343e9cb51b5405caf6c2844e3
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-06 09:31:03 -0700

    Add files via upload
    One Month Week 2 Lesson 8 Beginner Ruby strings and text

---
+++ OneMonth/omruby/strings.rb
@@ -0,0 +1,61 @@
+# Strings are text surrounded by quotes
+# Both single ('') and double ("") are used
+# examples: "dinosaurs", '2112', "I'm lovin' it!"
+
+yb_quote ="duh, Becky"
+puts yb_quote
+
+
+# use \  to esc long quotes or whena quote has a quote inside a quote
+
+hamilton_quote ="Well, the word got around, they said, \"This kid's insane, man\"" # That's how you escape text
+puts hamilton_quote
+
+kanye_quote ="My greatest pain in life \   
+is that I will never be able \              
+to see myself perform live." 
+puts kanye_quote
+
+# use """" for long text or strings
+Linkin_Park_One_More_Light ="""
+Should've stayed, were there signs, I ignored?
+Can I help you, not to hurt, anymore?
+We saw brilliance, when the world, was asleep
+There are things that we can have, but can't keep
+If they say
+Who cares if one more light goes out?
+In a sky of a million stars
+It flickers, flickers
+Who cares when someone's time runs out?
+If a moment is all we are
+We're quicker, quicker
+Who cares if one more light goes out?
+Well I do
+The reminders pull the floor from your feet
+In the kitchen, one more chair than you need oh
+And you're angry, and you should be, it's not fair
+Just 'cause you can't see it, doesn't mean it, isn't there
+If they say
+Who cares if one more light goes out?
+In a sky of a million stars
+It flickers, flickers
+Who cares when someone's time runs out?
+If a moment is all we are
+We're quicker, quicker
+Who cares if one more light goes out?
+Well I do
+Who cares if one more light goes out?
+In a sky of a million stars
+It flickers, flickers
+Who cares when someone's time runs out?
+If a moment is all we are
+We're quicker, quicker
+Who cares if one more light goes out?
+Well I do
+Well I do
+"""
+puts Linkin_Park_One_More_Light
+
+puts Linkin_Park_One_More_Light.reverse
+
+puts Linkin_Park_One_More_Light.upcase
