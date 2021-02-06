aefef0806d398dc0be7f63ecc1f8353479af8087
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-06 01:35:07 -0700

    Add files via upload
    second lesson coding of ruby /math

---
+++ OneMonth/omruby/Math.rb
@@ -0,0 +1,7 @@
+puts"Let's do some math!"
+puts "What is the answer to life, the universe, and everthing? #{30 + 20 - 8}"
+puts "Is it true that 5 *2 >5.0 /2"
+puts "What is 5 * 2? {5 *2}"
+puts "what is 5*2? #{5*2}"
+puts "what is 5/2? #{5/2}"
+puts "what is 5.0/2? #{5.0/2}"
