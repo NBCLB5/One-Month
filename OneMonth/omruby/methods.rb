8bb033c753127aa3168bd651884c07c30ddb70e8
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-07 19:46:46 -0700

    Add files via upload
    Week 3: Intermediate Ruby · Lesson 2

---
+++ OneMonth/omruby/methods.rb
@@ -0,0 +1,21 @@
+# variables are nouns
+# methods are verbs
+# example is . reverse or . upcase
+# methods start with def
+
+def print_two (thing, another_thing)
+    #...this is where the code goes
+    puts "thing_1: #{thing}, thing 2: #{another_thing}"
+end
+
+def print_one (thing)
+    puts "thing: #{thing}"
+end
+
+def Print_none (thing)
+    puts "I got nothing'"
+end
+
+print_two("Mattan", "Griffel")
+print_one("Yo!")
+print_none
