56620690b6c01a3ffc93b6e54269d307af41b158
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-08 19:54:30 -0700

    Update files.rb
    added L10 to L26
    Week 3: Intermediate Ruby · Lesson 6

--- OneMonth/omruby/files.rb
+++ OneMonth/omruby/files.rb
@@ -6,3 +6,21 @@ puts open("filename").read
 
 # or 
 puts ope("rules.txt")
+
+#writing
+file = open(filename, w)
+# W mode is danger danger
+print "what file do you what?"
+line1 = get.chomps
+
+print "really what file do you what?"
+line2 = get.chomps
+
+file.write(line1)
+file.write("/n")
+file.write(line2)
+
+file.close
+#careful if file exisit it wipes it out with the new file, if it dosent exsit it will create one.
+
+# mode "a" is a good mode for scraping
