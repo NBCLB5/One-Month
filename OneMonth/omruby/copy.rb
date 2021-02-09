121e2f8c4e5d12eda51a0032e750ed17c220be1d
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-09 09:06:12 -0700

    Add files via upload
    Week 3: Intermediate Ruby · Lesson 7

---
+++ OneMonth/omruby/copy.rb
@@ -0,0 +1,12 @@
+print "what is the name of the file that needs to be copied?"
+filename = "gets.chomp"
+file_content = open(filename).read
+
+print "where do you want to copy the file too?"
+
+
+new_file = open(new_filename, 'w')
+
+file.write(line1)  
+
+new_file.close
