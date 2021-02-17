4be9264562b6f29c801b4b658a66f0c389327775
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-16 23:04:26 -0700

    Add files via upload

---
+++ OneMonth/omruby/array.rb
@@ -0,0 +1,9 @@
+fruits = ["Apples", "Oranges", "Bananas"]
+people = [ 'Mattan', 'Chris'. 'Lee']
+things = [ "Dogs", 55, true, people]
+
+numbers = (1..10).to_a # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+puts "things are things: #{things}"
+puts "This is thing #1: #{things[0]}" # starts a 0
+puts "This is the last thing: #{things[-1]}" # can give you the last thing on the list
