b23fc7117cc9c4388e0fbaab7ebf09b3e4c5eb77
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-16 20:38:41 -0700

    Add files via upload

--- OneMonth/omruby/if.rb
+++ OneMonth/omruby/if.rb
@@ -1,1 +1,6 @@
- 
+ puts "Do you want to hear a joke? (Yes or No)" 
+ answer = gets.chomp
+
+ If answer == "Yes" || answer == "yes" || answer == "y" || answer == "Y"
+ puts "I'm against picking, but I don't know how to show it."
+end
