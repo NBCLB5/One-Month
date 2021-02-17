7289fce99b7318842dc7597c4a843eeb76ce37ec
N Banek <57334913+NBCLB5@users.noreply.github.com>
2021-02-16 21:52:53 -0700

    Add files via upload

--- OneMonth/omruby/if.rb
+++ OneMonth/omruby/if.rb
@@ -3,4 +3,13 @@
 
  If answer == "Yes" || answer == "yes" || answer == "y" || answer == "Y"
  puts "I'm against picking, but I don't know how to show it."
-end
+
+elsif answer == "no" || answer == "No" || answer = "N" || answer == "n"
+ puts "fine then"
+ 
+elsif answer == "Maybe" || answer == "maybe"
+    puts "what's green and has wheels? \nGrass, I lied about the wheels."
+else
+    puts "I don't understand."
+
+end 
